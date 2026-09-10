e4deeb6d228aee51a9aafa4796e625bab8c4e230
SamuelMB97 <43507135+SamuelMB97@users.noreply.github.com>
2026-09-10 14:24:35 -0600

    Added loadkeys generator

--- encode.py
+++ encode.py
@@ -15,38 +15,53 @@ def bin_to_hex(bin:str):
 
     
 def loadkeys(master_key64):
-    # I thought I had to remove every 8...
-    """temp = [c for c in master_key64]
-    print(f"{str_w_len(temp)} - temp")
-    for idx in [64, 56, 48, 40, 32, 24, 16, 8]:
-        temp.pop(idx-1)
-    
-    print(f"{str_w_len(temp)} - temp")
-
-    key_used56 = "".join(temp)
-    print(str_w_len(key_used56))
     """
-    k_perm_i = permute(master_key64, db.pc_1)
-    for j in [n+1 for n in range(16)]: # j = key round number
-        subkey56 = k_perm_i
-        yield subkey56
+    Takes a 64-bit master key and returns a generator for 16 28-bit
+    subkeys
+    """
+    k_perm_i_56 = permute(master_key64, db.pc_1, lossy=True)
+
+    c28 = k_perm_i_56[:28]
+    d28 = k_perm_i_56[28:]
+
+    def rotate_left(side):
+        return side[1:] + side[0]
+    
+    for round in [n+1 for n in range(16)]:
+        rotate_by = 2
+        if round in [1, 2, 9, 16]:
+            rotate_by = 1
+
+        for _ in range(rotate_by):
+            c28 = rotate_left(c28)
+            d28 = rotate_left(d28)
+
+        cd56 = c28 + d28
+        yield permute(cd56, db.pc_2, lossy=True)
+
+        
 
 
 def flatten_nested_list(nested_list):
     return [e for sublist in nested_list for e in sublist]
 
 
-def permute(bits, p_box):
+def permute(bits, p_box, lossy=False):
     """takes a string of bits, and a permutation box(as a list of
     lists), and permutes the bits by the box, returning a string of
-    bits."""
+    bits. Verifies lengths are equal first unless lossy=False"""
     p_list = flatten_nested_list(p_box)
-    assert len(p_list) == len(bits)
+    #print(f"{str_w_len(bits)} - bits")
+    #print(f"{str_w_len(p_list)} - p_list")
+    if not lossy:
+        assert len(p_list) == len(bits)
+    else:
+        assert len(p_list) <= len(bits)
     result_list = [] #turn the string of bits into a list of bits
 
-    for e in p_list:
-        idx = e-1
-        # print(f"{e} to {idx}. ", end="")
+    for old_pos in p_list:
+        idx = old_pos-1
+        # print(f"{bits[idx]} to {idx}. ", end="")
         result_list.append(bits[idx])
 
     result = ""
@@ -58,28 +73,28 @@ def permute(bits, p_box):
 
 def main(message, key):
     plaintext = message
-    k = key
+    subkeys = loadkeys(hex_to_bin(key))
 
     # Plaintext split into 64 bits
-    print(f"{plaintext} - plaintext")
+    #print(f"{plaintext} - plaintext")
     plain_as_bin = hex_to_bin(plaintext)
-    print(f"{plain_as_bin} - Plaintext split into 64 bits")
-    print(f"length of plaintext bits: {len(plain_as_bin)}")
+    #print(f"{plain_as_bin} - Plaintext split into 64 bits")
+    #print(f"length of plaintext bits: {len(plain_as_bin)}")
     
     # Initial permutation IP
     perm_i = permute(hex_to_bin(plaintext), db.p_i)
-    print(f"{str_w_len(perm_i)} - perm_i")
+    #print(f"{str_w_len(perm_i)} - perm_i")
 
     
     # Structure for code (blocks of 64, key of 56)
     # DES encryption - 16 rounds, different subkey each round
+
+    for subkey48 in subkeys:
+        print(f"{str_w_len(subkey48)} - subkey48") 
     # Each round:
         # Split L0 and R0 (64 --> 32, 32)
         # R0 copied to L1 (32 --> 32)
-        # Key transformation (54 --> 48)
-    key_bin = hex_to_bin(k)
-    print(f"{str_w_len(key_bin)} - key_bin")
-    subkey = loadkeys(key_bin)
+        # Key transformation (54 --> 48)      
             # details... (Learning about this on Wednesday) spits out K1 (48)
         # f_box with R0 and K1 (32, 48)
             # expansion (32 --> 48)
