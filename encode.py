2ae0f3318a9e62bf7f3b37b787b4a4cfcb364d2d
SamuelMB97 <43507135+SamuelMB97@users.noreply.github.com>
2026-09-11 17:52:31 -0600

    Cleaned up
    Added doc strings to functions without them. compacted a few lines for readability where it was unnecessarily expanded.

--- encode.py
+++ encode.py
@@ -8,7 +8,7 @@ def loadkeys(master_key64):
     Takes a 64-bit master key and returns a generator for 16 28-bit
     subkeys
     """
-    k_perm_i_56 = permute(master_key64, db.pc_1, change_size=True)
+    k_perm_i_56 = permute(master_key64, db.pc_1)
 
     c28 = k_perm_i_56[:28]
     d28 = k_perm_i_56[28:]
@@ -25,26 +25,20 @@ def loadkeys(master_key64):
             c28 = rotate_left(c28)
             d28 = rotate_left(d28)
 
-        cd56 = c28 + d28
-        yield permute(cd56, db.pc_2, change_size=True)
+        yield permute(c28 + d28, db.pc_2)
 
 
 def flatten_nested_list(nested_list):
+    """Takes a list of lists and returns a flattened list"""
     return [e for sublist in nested_list for e in sublist]
 
 
-def permute(bits, p_box, change_size=False):
+def permute(bits, p_box):
     """takes a string of bits, and a permutation box(as a list of
-    lists), and permutes the bits by the box, returning a string of
-    bits. Verifies lengths are appropriate according to change_size"""
+    lists), and permutes the bits by that box, returning a string of
+    permuted bits."""
 
     p_list = flatten_nested_list(p_box)
-
-    if change_size:
-        assert len(p_list) != len(bits)
-    else:
-        assert len(p_list) == len(bits)
-
     result_list = []
 
     for old_pos in p_list:
@@ -68,20 +62,22 @@ def as_subsets(bits, num_sets):
 
 
 def s_box_transform(bits6:list, s_box):
+    """takes 6 bits as a list and an s_box, returns a bit string corresponding to their
+    evaluation"""
     row = int(bits6[0] + bits6[5], 2)
     col = int("".join(bits6[1:5]), 2)
 
     val = s_box[row][col]
-    bits4 = f"{val:04b}"
-
-    return bits4
+    return f"{val:04b}"
 
 
 def run_s_boxes(subsets8_6):
-    s_box_idx = 0
+    """takes a list of 8 subsets of 6 bits each, puts each through the
+    appropriate s box, and returns a combined string of 32 bits"""
     s_boxes = [db.s_1, db.s_2, db.s_3, db.s_4, db.s_5, db.s_6, db.s_7, db.s_8]
     s_boxes_out8_4 = []
 
+    s_box_idx = 0
     for subset6 in subsets8_6:
         bits4 = s_box_transform(subset6, s_boxes[s_box_idx])
         s_boxes_out8_4.append(bits4)
@@ -91,17 +87,13 @@ def run_s_boxes(subsets8_6):
 
 
 def f_box(R32, subkey48):
-    bits48 = permute(R32, db.expand, change_size=True)
+    """Takes the Right 32 bits and the 48 bit subkey for the round,
+    and runs the f box on it, returning a new 32 bit string"""
+    bits48 = xor(permute(R32, db.expand), subkey48)
 
-    bits48 = xor(bits48, subkey48)
+    s_boxed32 = run_s_boxes(as_subsets(bits48, 6))
 
-    subsets8_6 = as_subsets(bits48, 6)
-
-    s_boxed32 = run_s_boxes(subsets8_6)
-
-    f_out32 = permute(s_boxed32, db.p)
-
-    return f_out32
+    return permute(s_boxed32, db.p)
     
 
 def xor(a, b):
@@ -120,20 +112,13 @@ def xor(a, b):
 
 
 def encode_DES_from_hex16(message_hex, key):
-
-    as_bin = con.hex_str_to_bin(message_hex)
-
-    bits64 = permute(as_bin, db.ip)
-
-
+    bits64 = permute(con.hex_str_to_bin(message_hex), db.ip)
     subkeys = loadkeys(con.hex_str_to_bin(key))
 
     L32 = bits64[:32]
     R32 = bits64[32:]
 
-    round_num = 0
     for subkey48 in subkeys:
-        round_num += 1
         temp = R32
 
         f_out32 = f_box(R32, subkey48)
@@ -141,13 +126,9 @@ def encode_DES_from_hex16(message_hex, key):
         R32 = xor(L32, f_out32)
         L32 = temp
 
-    swapped64 = R32 + L32
+    final_bits = permute(R32 + L32, db.ip_n1)
 
-    final_bits = permute(swapped64, db.ip_n1)
-
-    # Yield ciphertext"""
-    ciphertext = con.bin_str_to_hex(final_bits)
-    return ciphertext
+    return con.bin_str_to_hex(final_bits)
 
 
 def main(argv):
